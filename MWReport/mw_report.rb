5f081021d1b2519244aba95b8ea6d72f617b5e09
Donald Mennerich <donaldmennerich@nypl.org>
2013-11-07 21:07:41 -0500

    arrays of objects

--- MWReport/mw_report.rb
+++ MWReport/mw_report.rb
@@ -6,13 +6,50 @@ require 'sax-machine'
 class FileObject
 	include SAXMachine
 	element :filename
+	element :filesize
 	element :mediainfo_recognized
 	element :mediainfo_general_format
 	element :mediainfo_general_num_video_stream
 	element :mediainfo_general_num_audio_stream
+	element :mediainfo_general_duration
+	element :mediainfo_general_audio_format
+	element :mediainfo_general_video_format
+	element :mediainfo_general_audio_codec
+	element :mediainfo_general_video_codec
 end
 
+class MediaObject
+	@@total_vid_size = 0
+	@@total_vid_duration = 0
+	@@total_aud_size = 0
+	@@total_aud_duration = 0
+	
+	def initialize(filename, filesize, format, codec, duration, type)
+		@filename = filename
+		@filesize = filesize
+		@format = format
+		@codec = codec
+		@duration = duration
+		
+		if type == "VIDEO"
+			@@total_vid_size += filesize.to_i
+			@@total_vid_duration += duration.to_i
+		end
 
+		if type == "AUDIO"
+			@@total_aud_size += filesize.to_i
+			@@total_aud_duration += duration.to_i
+		end
+	end
+
+	def total_vid_duration
+		puts "hello"
+	end
+
+	def to_s
+   		@filename
+  	end
+end
 
 ##parse dfxml
 f = File.open("amia.xml")
@@ -26,9 +63,13 @@ while reader.read
 		fo = FileObject.parse(reader.outer_xml)
 		if fo.mediainfo_recognized == "true"
 			if fo.mediainfo_general_num_video_stream != "0"
-				videofiles << (fo.filename.to_s + "\t" + fo.mediainfo_general_format.to_s)
+				video_object = MediaObject.new(fo.filename, fo.filesize, fo.mediainfo_general_video_format, 
+					fo.mediainfo_general_video_codec, fo.mediainfo_general_duration, type = "VIDEO")
+				videofiles << video_object
 			elsif fo.mediainfo_general_num_audio_stream != "0"
-				audiofiles << (fo.filename.to_s + "\t" + fo.mediainfo_general_format.to_s)	
+				audio_object = MediaObject.new(fo.filename, fo.filesize, fo.mediainfo_general_audio_format, 
+					fo.mediainfo_general_audio_codec, fo.mediainfo_general_duration, type = "AUDIO")
+				audiofiles << audio_object
 			end
 		end
 	end
@@ -40,11 +81,15 @@ puts "media walker"
 puts "------------\n\n"
 
 puts "number of video files: " + videofiles.size.to_s
-videofiles.sort.each do |video|
-	puts "\t" + video
+puts "total duration of video files: "
+puts "total bytesize of video files: "
+videofiles.each do |video|
+	puts "\t" + video.to_s
 end
 
 puts "\nnumber of audio files: " + audiofiles.size.to_s
-audiofiles.sort.each do |audio|
-	puts "\t" + audio
+puts "total duration of audio files: "
+puts "total bytesize of audio files: "
+audiofiles.each do |audio|
+	puts "\t" + audio.to_s
 end
