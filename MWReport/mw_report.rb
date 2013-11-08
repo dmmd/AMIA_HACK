a6bcdec57feb57d857e6bba57aad49a8b9531fcb
Donald Mennerich <donaldmennerich@nypl.org>
2013-11-07 19:21:59 -0500

    changing gears parsing with ruby

---
+++ MWReport/mw_report.rb
@@ -0,0 +1,28 @@
+require "nokogiri"
+require 'sax-machine'
+
+module Dfxmlmi
+	module Parser
+		class FileObject
+      		include SAXMachine
+      		element :filename
+      		element :mediainfo_recognized
+      		element :mediainfo_format
+      	end
+	end
+end	
+
+
+f = File.open("amia.xml")
+reader = Nokogiri::XML::Reader(f)
+while reader.read
+	if reader.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT and reader.name == 'fileobject'
+		fo = Dfxmlmi::Parser::FileObject.parse(reader.outer_xml)
+		if fo.mediainfo_recognized == "true"
+			puts fo.filename.to_s + "\t" + ""
+		end
+	end
+end
+
+
+
